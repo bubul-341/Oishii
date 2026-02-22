0f89ae7cb515d01c2dd6f092b3132c82afc5d960
bubul-341 <bubulahmed341@gmail.com>
2026-02-22 15:03:07 +0530

    restaurant-page

--- FMS/src/main/java/com/FoodManagementSystem/ImageConfig.java
+++ FMS/src/main/java/com/FoodManagementSystem/ImageConfig.java
@@ -1,4 +1,16 @@
 package com.FoodManagementSystem;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class ImageConfig {
+@Configuration
+public class ImageConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:uploads/");
+    }
 }
+
